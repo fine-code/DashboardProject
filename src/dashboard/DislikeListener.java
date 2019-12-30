b51bd3690c403fffe2a64a528103f108dc5a840a
cszit <cszit@LAPTOP-NFTA9L2N.home>
2019-12-30 14:36:46 -0500

    fixed the dislike button
    since it wasn't totally working before

--- src/dashboard/DislikeListener.java
+++ src/dashboard/DislikeListener.java
@@ -11,40 +11,15 @@ public class DislikeListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-//			//if it's a like quote than just remove it from the like list and still show it sometimes
-//			if(LikeListener.getLikeQuotes().size() != 0 && ((LikeListener.getLikeQuotes().contains(likeQuotes.get(NextQuoteListener.getLikeQuoteUpTo() - 1))&& NextQuoteListener.getType() == "like") 
-//					|| (LikeListener.getLikeQuotes().contains(quotes.get(quoteUpTo - 1))&& NextQuoteListener.getType() == "Neutral") )){
-//				if(NextQuoteListener.getType() == "like") {
-//					if(likeQuotes.size() == 1) {
-//						likeQuotes.remove(NextQuoteListener.getLikeQuoteUpTo());
-//					}
-//					else {
-//						likeQuotes.remove(NextQuoteListener.getLikeQuoteUpTo() - 1);
-//						NextQuoteListener.setLikeQuoteUpTo(NextQuoteListener.getLikeQuoteUpTo() - 1);
-//					}
-//					NextQuoteListener.setQuoteType("Neutral");
-//				}
-//				else {
-//					if(likeQuotes.size() == 1) {
-//						likeQuotes.remove(likeQuotes.indexOf(quotes.get(quoteUpTo)));
-//					}
-//					else {
-//						likeQuotes.remove(likeQuotes.indexOf(quotes.get(quoteUpTo - 1)));
-//					}
-//					NextQuoteListener.setQuoteUpTo(quoteUpTo - 1); 
-//					
-//				}
-//			} 
-//			else {
-		quotes.remove(quoteIndex - 1);
 
-//				if(quotes.size() == 1) {
-//					quotes.remove(quoteUpTo);
-//				}
-//				else {
-//					quotes.remove(quoteUpTo - 1);
-//					NextQuoteListener.setQuoteUpTo(quoteUpTo - 1);
-//				}
+		if(quotes.size() == 1) {
+			quotes.remove(0);
+		}else if(quoteIndex == 0) {
+			quotes.remove(quotes.size() - 1);
+		}else {
+			quotes.remove(quoteIndex - 1);
+			NextQuoteListener.setquoteIndex(quoteIndex - 1);
+		}
 	}
 
 	public void setQuotes(ArrayList<Quote> quotes) {
